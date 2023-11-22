b8afe15f242bb25f6247259603f02b711a6dfc2f
Sandip854 <sandipk.rroy124@gmail.com>
2023-11-23 02:09:21 +0530

    no broker

---
+++ src/main/java/com/nobroker/payload/PropertyStatusUpdateRequest.java
@@ -0,0 +1,14 @@
+package com.nobroker.payload;
+
+public class PropertyStatusUpdateRequest {
+    private String status;
+
+    public String getStatus(){
+
+        return status;
+    }
+    public void setStatus(String status){
+
+        this.status=status;
+    }
+}
